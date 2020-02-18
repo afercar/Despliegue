316b1cb85654bff96ddac3c701f2fe4b54a0c16d
afercar <49988617+afercar@users.noreply.github.com>
2020-02-18 14:49:58 +0100

    Add files via upload

---
+++ DespliegueTrabajo/desplieguebbdd.sql
@@ -0,0 +1,18 @@
+﻿SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE DATABASE `desplieguebbdd`
+
+CREATE TABLE `usuarios` (
+  `nombre` varchar(50) COLLATE utf16_spanish2_ci DEFAULT NULL,
+  `pass` varchar(50) COLLATE utf16_spanish2_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish2_ci;
+
+
+INSERT INTO `usuarios` (`nombre`, `pass`) VALUES
+('lolo', 'pñol-Java'),
+('maria', 'asdf-php'),
+('Alejandro', '123123123123-Java');
+COMMIT;
