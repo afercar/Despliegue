4aeb9dbe9d5d4e18c49191bd5b82e46603dbb073
afercar <49988617+afercar@users.noreply.github.com>
2020-02-18 15:08:35 +0100

    Add files via upload

---
+++ DespliegueTrabajo/DesplieguePHP/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'host' => '192.168.1.145',
+    'username' => 'alex17',
+    'pass' => '1234',
+    'bbdd' => 'desplieguebbdd',
+    'puerto' => 3306
+);
+
+/*$conexion=mysqli_connect('192.168.1.145','alex17','1234','desplieguebbdd',3306);*/
+ ?>
+
+
+
